46d12880bd601aea7ddc937f10dced4be2c09557
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-02-10 15:02:49 +0200

    adds get on /accomodations

--- src/apis/accomodations/index.js
+++ src/apis/accomodations/index.js
@@ -1,12 +1,28 @@
 import express from "express";
+import createHttpError from "http-errors";
 import AccomodationsModel from "./model.js";
 const accomodationsRouter = express.Router();
 
 accomodationsRouter.post("/", async (req, res, next) => {
   try {
-    const newAccomodation = await AccomodationsModel(req.body);
-    const { _id } = await newAccomodation.save();
-    res.status(201).send({ _id });
+    if (req.body.host === "host") {
+      const newAccomodation = await AccomodationsModel(req.body);
+      const { _id } = await newAccomodation.save();
+      res.status(201).send({ _id });
+    } else {
+      next(createHttpError(400, "Please provide a host, not a guest"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+accomodationsRouter.get("/", async (req, res, next) => {
+  try {
+    const accomodations = await AccomodationsModel.find().populate({
+      path: "host",
+    });
+    res.send(accomodations);
   } catch (error) {
     next(error);
   }
