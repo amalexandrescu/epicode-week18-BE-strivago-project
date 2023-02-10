2f37842ab88b38b01fc42e4f34a7fd1805c57ade
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-02-10 13:08:39 +0200

    adds environment variables and completes server.js file with mongo connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import usersRouter from "./apis/users";
+import usersRouter from "./apis/users/index.js";
+import {
+  badRequestHandler,
+  forbiddenHandler,
+  genericErrorHAndler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
+import listEndpoints from "express-list-endpoints";
 
 const server = express();
 
@@ -17,3 +24,17 @@ server.use(express.json());
 server.use("/users", usersRouter);
 
 //ERROR HANDLERS
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(genericErrorHAndler);
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo!");
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server is running on port: ${port}`);
+  });
+});
